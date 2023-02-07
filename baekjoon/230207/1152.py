7c25b7352625decfd2f9a0282d5a0c7187562fe5
geunsinsa <asdf3161@naver.com>
2023-02-07 22:27:49 +0900

    230207

---
+++ baekjoon/230207/1152.py
@@ -0,0 +1,30 @@
+sentence = input().strip().split(' ')
+print(len(sentence))
+
+
+'''sentence = input().lower().split()
+arr = []
+for i in range(97, 123):
+    for j in range(len(sentence)):
+        for k in range(len(sentence[j])):
+            if sentence[j][k] == chr(i):
+                arr.append(chr(i))
+
+print(set(arr))
+
+
+
+
+total = 0
+a = remove.count(chr(97))
+print(a)
+
+for i in set(remove):
+    if remove.count(chr(i)) > 0:
+        total += 1
+
+print(total)'''
+
+
+
+
---
+++ baekjoon/230207/2908.py
@@ -0,0 +1,5 @@
+a, b = input().split()
+
+a = list(a)
+
+b = list(b)
